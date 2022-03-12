55d8fb06c47b5f37fdd77e80e78b01032905ac69
박원종 <wonjong1030@naver.com>
2022-03-12 12:49:11 +0900

    Feat: firstCommit

---
+++ src/main/java/com/preemptivebookcafe/PreemptiveBookcafeApplication.java
@@ -0,0 +1,13 @@
+package com.preemptivebookcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreemptiveBookcafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreemptiveBookcafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/preemptivebookcafe/PreemptiveBookcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preemptivebookcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreemptiveBookcafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
