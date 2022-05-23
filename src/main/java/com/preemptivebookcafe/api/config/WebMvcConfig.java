e177f425e390824aad8c042010d0e87d879f29f2
박원종 <wonjong1030@naver.com>
2022-05-23 21:09:05 +0900

    Feat: Cors 설정 추가

--- src/main/java/com/preemptivebookcafe/api/config/WebMvcConfig.java
+++ src/main/java/com/preemptivebookcafe/api/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.preemptivebookcafe.api.config;
 import com.preemptivebookcafe.api.interceptor.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor).addPathPatterns("/*");
         //.excludePathPatterns("/경로명") --> 이 경로는 인터셉터가 가로채는 것에서 제외됨.
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
     }
 }
